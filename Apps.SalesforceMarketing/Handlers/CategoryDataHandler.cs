a9e3ffca1a4218f7bde7d491b905eaef87c0b0b6
Anton Fesenko <tohafesenkolovep@gmail.com>
2026-01-14 12:09:46 +0200

    add virtual root (category with the id of 0) to CategoryDataHandler

--- Apps.SalesforceMarketing/Handlers/CategoryDataHandler.cs
+++ Apps.SalesforceMarketing/Handlers/CategoryDataHandler.cs
@@ -9,7 +9,7 @@ namespace Apps.SalesforceMarketing.Handlers;
 public class CategoryDataHandler(InvocationContext invocationContext)
     : SalesforceInvocable(invocationContext), IAsyncFileDataSourceItemHandler
 {
-    private const int RootFolderId = 0;
+    private const int GlobalRootFolderId = 0;
 
     public async Task<IEnumerable<FileDataItem>> GetFolderContentAsync(FolderContentDataSourceContext context, CancellationToken ct)
     {
@@ -18,41 +18,46 @@ public class CategoryDataHandler(InvocationContext invocationContext)
 
         if (!string.IsNullOrEmpty(context.FolderId))
             filters.Add($"parentId eq {context.FolderId}");
-        else if (string.IsNullOrEmpty(context.FolderId))
-            filters.Add($"parentId eq {RootFolderId}");
+        else
+            filters.Add($"parentId eq {GlobalRootFolderId}");
 
         if (filters.Count != 0)
             request.AddQueryParameter("$filter", string.Join(" AND ", filters));
 
         var response = await Client.PaginateGet<CategoryEntity>(request);
-        var folders = new List<FileDataItem>();
 
-        foreach (var category in response)
+        var folders = new List<FileDataItem>();
+        foreach (var folder in response)
         {
-            var folder = new Folder
+            var category = new Folder()
             {
-                Id = category.Id.ToString(),
-                DisplayName = category.Name,
+                Id = folder.Id.ToString(),
+                DisplayName = folder.Name,
                 IsSelectable = true
             };
-            folders.Add(folder);
+            folders.Add(category);
         }
+
         return folders;
     }
 
     public async Task<IEnumerable<FolderPathItem>> GetFolderPathAsync(FolderPathDataSourceContext context, CancellationToken ct)
     {
         if (string.IsNullOrEmpty(context.FileDataItemId) || !int.TryParse(context.FileDataItemId, out int currentId))
-            return [new FolderPathItem { Id = RootFolderId.ToString(), DisplayName = "Content Builder" }];
+            return [new FolderPathItem { Id = GlobalRootFolderId.ToString(), DisplayName = "Home" }];
 
         var path = new List<FolderPathItem>();
+
+        int? startNodeParentId = null;
         while (currentId != 0)
         {
             var request = new RestRequest($"asset/v1/content/categories/{currentId}", Method.Get);
-
             var category = await Client.ExecuteWithErrorHandling<CategoryEntity>(request);
+
             if (category == null) 
                 break;
+
+            startNodeParentId ??= category.ParentId;
 
             path.Add(new FolderPathItem
             {
@@ -63,7 +68,9 @@ public class CategoryDataHandler(InvocationContext invocationContext)
             currentId = category.ParentId;
         }
 
-        if (path.Count > 0)
+        path.Add(new FolderPathItem { Id = GlobalRootFolderId.ToString(), DisplayName = "Home" });
+
+        if (path.Count > 0 && startNodeParentId != 0)
             path.RemoveAt(0);
 
         path.Reverse();
